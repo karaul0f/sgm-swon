491277911cef99356dfa81252f16539a6720ac26
antour <andbobngs.ru@gmail.com>
2021-11-06 17:52:21 +0700

    Revert "Вернул отбражение кнопок"
    This reverts commit eac50fdce62e85544b9c7ba558b0b293df4ec63d.

--- Assets/Client/Scripts/EventHandlers/AppHandler.cs
+++ Assets/Client/Scripts/EventHandlers/AppHandler.cs
@@ -21,7 +21,7 @@ public class AppHandler : MonoBehaviour
     void OnTabChangedHandler(TabManager.Tab newTabValue)
     {
         gameObject.SetActive(newTabValue == m_targetApplication);
-        for (int i = 1; i < gameObject.transform.childCount - 2; i++)
+        for (int i = 1; i < gameObject.transform.childCount; i++)
         {
             gameObject.transform.GetChild(i).gameObject.SetActive(false);
         }
