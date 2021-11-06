59949e2f4be028cb7ef8596ff159f0d54aff979e
UhBroSeemOff <fantomklin@outlook.com>
2021-11-06 18:52:13 +0700

    Интеграция DI Добавлен пакет Zenject Создан интерфейс сервиса путешествий Создан примитивный сервис путешествий Сервис проинжекчен в TourManager в качестве теста Апдейт манифеста Апдейт .gitignore

--- Assets/Client/Scripts/TourManager.cs
+++ Assets/Client/Scripts/TourManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Client.Scripts.Services.Interfaces;
 using UnityEngine;
+using Zenject;
 
 public class TourManager : MonoBehaviour
 {
     public GameObject GameManager;
+
+    private ITravelService _travelService;
+
+    // TODO: Убрать за ненадобностью. Пример использования.
+    [Inject]
+    public void Construct(ITravelService travelService)
+    {
+        _travelService = travelService;
+    }
 
     // Start is called before the first frame update
     void Start()
